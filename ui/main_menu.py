14fd052080c59cb5d9cbd4d50071e90435da6268
DerKumpel <tmbenkenstein@gmail.com>
2026-06-21 13:57:32 +0200

    Remove commented-out text rendering code from MainMenu update method

--- ui/main_menu.py
+++ ui/main_menu.py
@@ -16,9 +16,11 @@ class MainMenu:
     def update(self):
         pr.begin_drawing()
         pr.clear_background(pr.BLACK)
-        text = "Hello from PYRO ENGINE"
-        text_x, text_y = center_text(text, self.win_width, self.win_height, 20)
-        pr.draw_text(text, text_x, text_y, 20, pr.RED)
+        # text = "Hello from PYRO ENGINE"
+        # text_x, text_y = center_text(text, self.win_width, self.win_height, 20)
+        # pr.draw_text(text, text_x, text_y, 20, pr.RED)
+        if(pr.gui_button([10, 10, 120, 30], "Test")):
+            self.should_close = True
         pr.end_drawing()
 
     def handle_input(self):
