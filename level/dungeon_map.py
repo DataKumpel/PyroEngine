8fe25d82d3530136170890998cfce53ac871456b
DerKumpel <tmbenkenstein@gmail.com>
2026-06-28 19:46:29 +0200

    Remove collision attribute from MapCell class and update serialization methods

--- level/dungeon_map.py
+++ level/dungeon_map.py
@@ -9,16 +9,20 @@ logger = logging.getLogger("PYRO_ENGINE")
 class MapCell:
 
     # Serialization fields:
-    #         0    1    2    3      4      5
-    serial = "base wall ceil detail entity col".split()
+    #         0    1    2    3      4
+    serial = "base wall ceil detail entity".split()
 
-    def __init__(self, base_layer=0, wall_layer=0, ceiling_layer=0, detail_layer: list | None = None, entity_id=0, collision=False):
+    def __init__(self, 
+                 base_layer=0, 
+                 wall_layer=0, 
+                 ceiling_layer=0, 
+                 detail_layer: list | None = None, 
+                 entity_id=0):
         self.base_layer = base_layer
         self.wall_layer = wall_layer
         self.ceiling_layer = ceiling_layer
         self.detail_layer = detail_layer or []
         self.entity_id = entity_id
-        self.collision = collision
 
     def to_dict(self) -> dict:
         return {
@@ -27,7 +31,6 @@ class MapCell:
             self.serial[2]: self.ceiling_layer,
             self.serial[3]: self.detail_layer,
             self.serial[4]: self.entity_id,
-            self.serial[5]: self.collision,
         }
     
     @classmethod
@@ -38,7 +41,6 @@ class MapCell:
             ceiling_layer=data.get(cls.serial[2], 0),
             detail_layer=data.get(cls.serial[3], []),
             entity_id=data.get(cls.serial[4], 0),
-            collision=data.get(cls.serial[5], False),
         )
     
 
